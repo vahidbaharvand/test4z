f288c25447ead5d380f70b00b9d857e2126f9ca9
Hizir Cagin Bektas <hizir-cagin.bektas@broadcom.com>
2022-01-24 14:26:22 +0100

    http client error handling

--- src/python/src/main/service/HttpClient.py
+++ src/python/src/main/service/HttpClient.py
@@ -3,15 +3,18 @@ import json
 from utility import get_test4z_connection
 
 def post_request(method, payload):
-      print("\nTest4z " + method + " request...")
-      SESSION = get_test4z_connection()
-      HEADERS={'content-type' : "application/json"}
-      response = requests.post(
-        SESSION["url"] + method,
-        data=payload,
-        auth=(SESSION["user"], SESSION["password"]),
-        headers=HEADERS,
-        verify=SESSION["ssl_verification"],
-        timeout=SESSION["timeout"]
-      )
-      return response.json()
+    try:
+        print("\nTest4z " + method + " request...")
+        SESSION = get_test4z_connection()
+        HEADERS={'content-type' : "application/json"}
+        response = requests.post(
+            SESSION["url"] + method,
+            data=payload,
+            auth=(SESSION["user"], SESSION["password"]),
+            headers=HEADERS,
+            verify=SESSION["ssl_verification"],
+            timeout=SESSION["timeout"]
+        )
+        return response.json()
+    except Exception:
+            raise Exception("Unexpected error")
