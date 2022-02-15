3def314fbb78e34f199ca9107fa6cd34b1a148ba
Hizir Cagin Bektas <hizir-cagin.bektas@broadcom.com>
2022-02-15 14:45:36 +0100

    unnecessary import removed

--- src/test/DiagnosticCheck.ts
+++ src/test/DiagnosticCheck.ts
@@ -1,4 +1,4 @@
-import { Test4zService, DiagnosticResult } from "@broadcom/test4z";
+import { Test4zService } from "@broadcom/test4z";
 async function main() {
     const diagnostic = await Test4zService.diagnostic();
     diagnostic && diagnostic.data ? console.table(diagnostic.data) : console.log(diagnostic)
