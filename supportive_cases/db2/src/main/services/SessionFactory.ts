20a402b8273579a4c07e13323ef030316391710d
Hizir Cagin Bektas <hizir-cagin.bektas@broadcom.com>
2022-04-25 16:25:30 +0200

    code duplicate removed

--- supportive_cases/db2/src/main/services/SessionFactory.ts
+++ supportive_cases/db2/src/main/services/SessionFactory.ts
@@ -1,5 +1,6 @@
 import {IDB2Session} from "@zowe/db2-for-zowe-cli/lib";
-import {IProfArgAttrs, ProfileInfo} from "@zowe/imperative";
+import {ProfileInfo} from "@zowe/imperative";
+import { SessionFactory as Test4zSessionFactory }  from "@broadcom/test4z";
 
 
 export class SessionFactory {
@@ -9,63 +10,30 @@ export class SessionFactory {
      * Returns the DB2 profile by reading the information from the zowe config file, under the db2 tag
      */
     public static async getDb2Profile(): Promise<IDB2Session> {
-        const _hostname = await SessionFactory.getProfileProp("db2", "host")
-        const _port = await this.getProfileProp("db2", "port")
-        const _database = await this.getProfileProp("db2", "database")
-        const _user = await this.getProfileProp("db2", "user")
-        const _password = await this.getProfileProp("db2", "password")
+        const _hostname = await Test4zSessionFactory.getProfileProp("db2", "host")
+        const _port = await Test4zSessionFactory.getProfileProp("db2", "port")
+        const _database = await Test4zSessionFactory.getProfileProp("db2", "database")
+        const _user = await Test4zSessionFactory.getProfileProp("db2", "user")
+        const _password = await Test4zSessionFactory.getProfileProp("db2", "password")
 
         const session: IDB2Session = {
-        hostname: _hostname ? _hostname : "",
-        port: _port ? _port : 0,
-        user: _user ? _user : "",
-        password: _password ? _password : "",
-        database: _database ? _database : "",
+            hostname: _hostname ? _hostname : "",
+            port: _port ? _port : 0,
+            user: _user ? _user : "",
+            password: _password ? _password : "",
+            database: _database ? _database : "",
         };
         return session;
     }
 
+
+
     /**
-     * Get the properties for test4z profile
+     * Get the properties for the given profile
      * @param property: property name
      * @returns any: Returns the value of the property
      */
     public static async getProfileProp(profileName: string, property : any) {
-        let val;
-        let args = await this.getProfileArgs(profileName);
-        args.forEach(arg =>{
-            if(arg.argName == property){
-                val = arg.argValue;
-            }
-        })
-        return val;
-    }
-
-    private static async getProfileArgs(profileName : string): Promise<IProfArgAttrs[]>{
-        let profileInfo = await SessionFactory.getProfInfo();
-        const defaultProfile = profileInfo.getDefaultProfile(profileName);
-        let arg = profileInfo.mergeArgsForProfile(defaultProfile, { getSecureVals: true });
-        return arg.knownArgs;
-    }
-
-    /**
-     * Get the properties of the profile
-     * @param profileName: Profile name
-     * @returns IProfArgAttrs[]: Returns the properties as the array of arguments
-     */
-    private static async getProfInfo(): Promise<ProfileInfo>{
-        if(!this.profInfo){
-            this.profInfo =  this.getProfilePromise();
-        }
-        return this.profInfo;
-    }
-
-    private static async getProfilePromise() : Promise<ProfileInfo>{
-        let profInfoOpts = {
-            overrideWithEnv: true
-        }
-        let zoweProf = new ProfileInfo("zowe", profInfoOpts);
-        await zoweProf.readProfilesFromDisk();
-        return zoweProf;
+        return await Test4zSessionFactory.getProfileProp(profileName, property)
     }
 }
