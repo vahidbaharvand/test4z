a46878cfb783c8acda83fcb85579ccd8ba4a07c5
Hizir Cagin Bektas <hizir-cagin.bektas@broadcom.com>
2022-05-05 11:28:39 +0200

    removing protocol for zosmf

--- .scripts/setZosmf.sh
+++ .scripts/setZosmf.sh
@@ -3,8 +3,6 @@ set -e
 printf "\nPlease enter Z/OSMF specific information when the command prompt asks\n"
 npx zowe config set profiles.lpar1.profiles.zosmf.properties.host
 npx zowe config set profiles.lpar1.profiles.zosmf.properties.port
-printf "(set http or https for the next command)\n"
-npx zowe config set profiles.lpar1.profiles.zosmf.properties.protocol
 printf "\nEnter your mainframe username and password. To secure your credentials, the information you enter will be blank\n"
 npx zowe config set --secure profiles.lpar1.profiles.zosmf.properties.user
 npx zowe config set --secure profiles.lpar1.profiles.zosmf.properties.password
