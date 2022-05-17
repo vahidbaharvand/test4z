d465c4d52bf3b0f1934c810316ecfc47a0b4282d
Vibhor Agarwal <vibhoremohandas.agarwal@broadcom.com>
2022-05-17 12:41:21 +0200

    remove unused imports

--- supportive_cases/ca-view/src/test/caviewsample.test.ts
+++ supportive_cases/ca-view/src/test/caviewsample.test.ts
@@ -2,9 +2,7 @@ import {Repositories, IRepository, Reports, IReport, ViewRestClient} from "@broa
 import {IResult} from "../main/services/IResult";
 import {Profiles, SessionFactory} from "@broadcom/test4z";
 import {Session} from "@zowe/imperative";
-import { Test4zService,Filter, Operators, Types, QueryOperators, FilterBuilder } from "@broadcom/test4z";
 import {IJob, SubmitJobs, GetJobs, IJobFile} from "@zowe/cli";
-import { assert } from "console";
 
 let JCLDSName = "PTCINCUB.SPITFIRE.JCL(T4ZVIEW)";
  
@@ -14,14 +12,14 @@ test("Submit a job through ZOSMF and retrieve the contents", async function () {
     const job: IJob = await SubmitJobs.submitJobNotify(zOSMFSession, JCLDSName);
     expect(job.retcode).toMatch("CC 0000");
 
-    let spoolcontent = {};
+    let spoolContent = "";
     const jobLogs: IJobFile[] = await GetJobs.getSpoolFilesForJob(zOSMFSession, job);
     for(const jobLog of jobLogs){
         if(jobLog.ddname === "SORTOUT"){
-            spoolcontent = await GetJobs.getSpoolContent(zOSMFSession,jobLog);
+            spoolContent = await GetJobs.getSpoolContent(zOSMFSession,jobLog);
         }
     }
-    expect(spoolcontent).toContain("ALL RECORDS ARE SORTED");
+    expect(spoolContent).toContain("ALL RECORDS ARE SORTED");
 
 });
 
@@ -31,16 +29,16 @@ test("Submit a job through ZOSMF and retrieve the contents", async function () {
     //     let repositoryList:IRepository[] =  await repository.list();
     //     expect(repositoryList.length).toBeGreaterThan(0);
     //     let repositoryID :number = repositoryList[0].id
-        
+    //
     //     let reports:Reports = new Reports(session,repositoryID);
     //     let report:IReport[] = await reports.listReports();
     //     expect(report.length).toBeGreaterThan(0);
-    //     let reportHandleID :string = report[19].handle; 
-    
+    //     let reportHandleID :string = report[19].handle;
+    //
     //     let responsetxt:IResult = await ViewRestClient.getExpectJSON(session,"/v1/view/rptdata/"+repositoryID+"/"+reportHandleID) as IResult;
     //     let responsestring = "";
     //     for (let reportData of responsetxt.result["Report Data"]) {
     //         responsestring += reportData.data + "\n";
-    //      }  
+    //      }
     //     console.log("----JOB LOGS---- \n",responsestring);
-    //});
+    // });
